fb28a56385c130bf0516dd7b0e83e347c72b4b50
TobiasKutscher <tobias-kutscher@web.de>
2025-10-09 17:21:25 +0200

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,9 @@ import Script from 'next/script'
 export const metadata = {
   title: 'Tobias Kutscher - Senior Data Scientist & AI Engineer',
   description: 'Senior Data Scientist Portfolio and Side Hustles',
+  icons: {
+    icon: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨🏻‍💻</text></svg>"
+  }
 }
 
 const lato = Lato({
